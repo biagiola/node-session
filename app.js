51aed7800a0d169d9c81e0f0becbe0a7b2ac3021
David <41311088+biagiola@users.noreply.github.com>
2020-07-07 16:50:02 -0400

    database connection added

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const express = require('express')
 const session = require('express-session')
 const users = require('./fakeDatabase')
 const app = express()
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
 
 app.use(express.urlencoded({extended: true})); //bodyparser
+
+// database connection
+mongoose.connect( 'mongodb://localhost/nodesession', 
+    { 
+        useNewUrlParser: true, 
+        useCreateIndex: true, 
+        useUnifiedTopology: true, 
+        useFindAndModify: false 
+    })
+mongoose.connection.once('open', () => {
+    console.log('Connection with the database has been made')
+}).on('error', (error) => {
+    console.log('Connection database error:', error)
+});  
 
 const {PORT, NODE_ENV, SESS_NAME, SESS_SECRET, SESS_LIFETIME}  = require('./sessionConfig')
 //set node-session configurations
@@ -12,7 +28,7 @@ app.use(session({
     resave: false, //save even if the session was never modified 
     saveUninitialized: false, //force a session that is uninitialized to be saved to the store
     secret: SESS_SECRET,
-    //store: mongodb
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
     cookie: {
         maxAge: SESS_LIFETIME, //maxime time for a session 
         sameSite: true, //stric
